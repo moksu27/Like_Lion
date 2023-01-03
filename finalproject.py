78c08ad9e75305b010b446eff3ee71f19f35dcdf
moksu27 <>
2023-01-03 17:29:16 +0900

    data

--- finalproject.py
+++ finalproject.py
@@ -11,11 +11,15 @@ st.set_page_config(
     layout="wide",
 )
 # 데이터 불러오기
-url = ""
+url_20 = "https://raw.githubusercontent.com/moksu27/Pet_industry_prediction_streamlit/main/data/news20_%EC%A0%84%EC%B2%98%EB%A6%AC.csv"
+url_21 = "https://raw.githubusercontent.com/moksu27/Pet_industry_prediction_streamlit/main/data/news21_%EC%A0%84%EC%B2%98%EB%A6%AC.csv"
+url_22 = "https://raw.githubusercontent.com/moksu27/Pet_industry_prediction_streamlit/main/data/news22_%EC%A0%84%EC%B2%98%EB%A6%AC.csv"
 
-df = pd.read_csv(url)
+df_20 = pd.read_csv(url_20)
+df_21 = pd.read_csv(url_21)
+df_22 = pd.read_csv(url_22)
 
 
 # 데이터 출력
-st.title('')
-st.dataframe(df)
+st.title('펫 산업 관련 기사 데이터')
+st.dataframe(df_20)
