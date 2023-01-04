53d832acb868f1e99cd75f671a7a23d0619e0136
moksu27 <>
2023-01-05 03:02:02 +0900

    save

--- pages/3펫 산업 분석 결과.py
+++ pages/3펫 산업 분석 결과.py
@@ -31,19 +31,30 @@ def run_query():
 
         # Get views for first workbook.
         server.workbooks.populate_views(workbooks[6])
+        server.workbooks.populate_views(workbooks[7])
+
  # Get image of workbook.
         item1 = workbooks[6].views[0]
         server.views.populate_image(item1)
         img1 = item1.image
         
-        return workbooks_names, img1
-workbooks_names, img1 = run_query()
+        item2 = workbooks[7].views[0]
+        server.views.populate_image(item2)
+        img2 = item2.image
+        
+        return workbooks_names, img1, img2
+workbooks_names, img1, img2 = run_query()
     
 image1 = Image.open('data/연도별 tf-idf 합계 비중.png')
 image2 = Image.open('data/스타사업.png')
 image3 = Image.open('data/word2vec.png')
+image4 = Image.open('data/tfidf.png')
 
+st.header("연도별 Tf-idf 합계 비중 (특허 기준)")
+st.image(image4, width = 800)
+st.image(img2, width = 400)
 
+st.header("연도별 Tf-idf 합계 비중 (뉴스기사 기준)")
 st.image(image1, width = 800)
 st.image(img1, width = 400)
 
