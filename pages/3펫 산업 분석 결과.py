3fd8bc120daba29ebcd06c7d910c6af2bd841733
moksu27 <>
2023-01-05 02:47:13 +0900

    save

--- pages/3펫 산업 분석 결과.py
+++ pages/3펫 산업 분석 결과.py
@@ -4,16 +4,48 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 from PIL import Image
+import tableauserverclient as TSC
+
 
 st.title('펫 산업 동향 분석 결과')
 
+# Set up connection.
+tableau_auth = TSC.PersonalAccessTokenAuth(
+    st.secrets["tableau"]["token_name"],
+    st.secrets["tableau"]["personal_access_token"],
+    st.secrets["tableau"]["site_id"],
+)
+server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
+
+
+# Get various data.
+# Explore the tableauserverclient library for more options.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query():
+    with server.auth.sign_in(tableau_auth):
+
+        # Get all workbooks.
+        workbooks, pagination_item = server.workbooks.get()
+        workbooks_names = [w.name for w in workbooks]
+
+        # Get views for first workbook.
+        server.workbooks.populate_views(workbooks[6])
+ # Get image of workbook.
+        item1 = workbooks[6].views[0]
+        server.views.populate_image(item1)
+        img1 = item1.image
+        
+        return workbooks_names, img1
+workbooks_names, img1 = run_query()
+    
 image1 = Image.open('data/연도별 tf-idf 합계 비중.png')
 image2 = Image.open('data/스타사업.png')
 image3 = Image.open('data/word2vec.png')
 
 
 st.image(image1, width = 800)
-
+st.image(img1, width = 800)
 
 
 st.image(image2, width = 800)
