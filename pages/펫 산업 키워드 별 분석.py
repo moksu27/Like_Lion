596e8a830ea123ccfa60b713c74c2d642d9670f9
moksu27 <>
2023-01-05 01:02:56 +0900

    save

--- pages/펫 산업 키워드 별 분석.py
+++ pages/펫 산업 키워드 별 분석.py
@@ -34,21 +34,62 @@ def run_query():
         server.views.populate_image(view_item1)
         view_image1 = view_item1.image
         
-        view_item2 = workbooks[2].views[4]
+        view_item2 = workbooks[2].views[1]
         server.views.populate_image(view_item2)
         view_image2 = view_item2.image
         
-        return workbooks_names, view_image1, view_image2
+        view_item3 = workbooks[2].views[2]
+        server.views.populate_image(view_item3)
+        view_image3 = view_item3.image
+        
+        view_item4 = workbooks[2].views[3]
+        server.views.populate_image(view_item4)
+        view_image4 = view_item4.image
+        
+        view_item5 = workbooks[2].views[4]
+        server.views.populate_image(view_item5)
+        view_image5 = view_item5.image
+        
+        view_item6 = workbooks[2].views[5]
+        server.views.populate_image(view_item6)
+        view_image6 = view_item6.image
+        
+        view_item7 = workbooks[2].views[6]
+        server.views.populate_image(view_item7)
+        view_image7 = view_item7.image
+        
+        view_item8 = workbooks[2].views[7]
+        server.views.populate_image(view_item8)
+        view_image8 = view_item8.image
+        
+        view_item9 = workbooks[2].views[8]
+        server.views.populate_image(view_item9)
+        view_image9 = view_item9.image
+        
+        return workbooks_names, view_image1, view_image2, view_image3, view_image4, view_image5, view_image6, view_image7, view_image8, view_image9
 
-workbooks_names, view_image1, view_image2 = run_query()
+workbooks_names, view_image1, view_image2, view_image3, view_image4, view_image5, view_image6, view_image7, view_image8, view_image9 = run_query1()
 
+word = st.selectbox('펫 산업 키워드',['반려동물 용품','펫케어','펫푸드', '종합'])
 
-# Print results.
 st.subheader(workbooks_names[2])
 st.write("Found the following workbooks:", ", ".join(workbooks_names))
 
-st.subheader("반려동물 용품")
-st.image(view_image1, width=800)
-st.image(view_image2, width=800)
+if word == '반려동물 용품':
+    st.subheader("반려동물 용품")
+    st.image(view_image1, width=800)
+    st.image(view_image5, width=800)
+    
+elif word == '펫케어':
+    st.subheader("반려동물 용품")
+    st.image(view_image2, width=800)
+    st.image(view_image6, width=800)
+elif word == '펫푸드':
+    st.subheader("반려동물 용품")
+    st.image(view_image3, width=800)
+    st.image(view_image7, width=800)
 
-# st.image(view_image5, width=1000, height = 500)
+elif word == '종합':
+    st.subheader("종합")
+    st.image(view_image4, width=800)
+    st.image(view_image9, width=800)
