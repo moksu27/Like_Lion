04e1a1906393fae9924545332e96bb4e526eddd1
moksu27 <>
2023-01-05 02:07:29 +0900

    save

--- pages/펫 산업 키워드 별 분석.py
+++ pages/펫 산업 키워드 별 분석.py
@@ -129,20 +129,24 @@ st.write("Found the following workbooks:", ", ".join(workbooks_names))
 
 if word == '반려동물 용품':
     st.subheader("반려동물 용품")
+    st.image(img16, width=800)
+    st.image(img5, width=800)
+    st.caption('반려동물 용품 관련 키워드 (Tf-idf 기준): 2020-2022 총합')
+    st.image(img17, width=800)
+    st.image(img1, width=800)
     st.image(img14, width=800)
     st.caption("반려동물 용품 관련 키워드 (Tf-idf 기준): 연도별")
-    st.image(img17, width=800)
-    st.image(img5, width=800)
-    st.caption('펫푸드 관련 키워드 (Tf-idf 기준): 2020-2022 총합')
-    st.image(img1, width=800)
-
     
 elif word == '펫케어':
     st.subheader("펫케어")
+    st.image(img12, width=800)
     st.image(img6, width=800)
-    st.image(img11, width=800)
+    st.caption('펫케어 관련 키워드 (Tf-idf 기준): 2020-2022 총합')
+    st.image(img13, width=800)
     st.image(img2, width=800)
-
+    st.image(img10, width=800)
+    st.caption("펫케어 관련 키워드 (Tf-idf 기준): 연도별")
+    
     
 elif word == '펫푸드':
     st.subheader("펫푸드")
